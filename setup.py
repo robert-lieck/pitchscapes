012b77e772e9fece4f78432b960a8b6a541b0c28
Robert Lieck <science@robert-lieck.com>
2020-10-30 21:38:53 +0100

    prepare for build

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pitchscapes",
+    version="0.1.2",
+    author="Robert Lieck",
+    author_email="robert.lieck@epfl.ch",
+    description="computing pitch-scapes and pitch-scape clusters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DCMLab/pitchscapes",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
