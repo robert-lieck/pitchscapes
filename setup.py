2c448dab825559323c6e78646185226d14b20ba2
Robert Lieck <science@robert-lieck.com>
2020-10-30 22:07:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pitchscapes",
-    version="0.1.3",
+    version="0.1.4",
     author="Robert Lieck",
     author_email="robert.lieck@epfl.ch",
     description="computing pitch-scapes and pitch-scape clusters",
